6b9b1b220f212b67ca6e2160ba72ec05084041c0
xener <xener@DESKTOP-C5ONBIJ>
2018-03-21 02:18:13 -0400

    Add project files.

---
+++ AverageUnitsShippedByEmployee_ScottBrown/AverageUnitsShippedByEmployee.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageUnitsShippedByEmployee
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblDayDisplay = New System.Windows.Forms.Label()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.txtInputBox = New System.Windows.Forms.TextBox()
+        Me.lblEmployeeOne = New System.Windows.Forms.Label()
+        Me.lblEmpoyeeTwo = New System.Windows.Forms.Label()
+        Me.lblEmployeeThree = New System.Windows.Forms.Label()
+        Me.txtEmployeeOneResultDisplay = New System.Windows.Forms.TextBox()
+        Me.txtEmployeeTwoResultDisplay = New System.Windows.Forms.TextBox()
+        Me.txtEmployeeThreeResultDisplay = New System.Windows.Forms.TextBox()
+        Me.lblEmployeeOneAverage = New System.Windows.Forms.Label()
+        Me.lblEmployeeTwoAverage = New System.Windows.Forms.Label()
+        Me.lblEmployeeThreeAverage = New System.Windows.Forms.Label()
+        Me.lblEmployeeTotalAverage = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.ttAverageEmployeeUnitsShipped = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblDayDisplay
+        '
+        Me.lblDayDisplay.Location = New System.Drawing.Point(12, 18)
+        Me.lblDayDisplay.Name = "lblDayDisplay"
+        Me.lblDayDisplay.Size = New System.Drawing.Size(45, 17)
+        Me.lblDayDisplay.TabIndex = 0
+        Me.lblDayDisplay.Text = "Day 1"
+        Me.lblDayDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblUnits
+        '
+        Me.lblUnits.Location = New System.Drawing.Point(12, 51)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(44, 17)
+        Me.lblUnits.TabIndex = 1
+        Me.lblUnits.Text = "&Units:"
+        Me.lblUnits.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtInputBox
+        '
+        Me.txtInputBox.Location = New System.Drawing.Point(77, 51)
+        Me.txtInputBox.Name = "txtInputBox"
+        Me.txtInputBox.Size = New System.Drawing.Size(69, 22)
+        Me.txtInputBox.TabIndex = 2
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.txtInputBox, "Enter the amount of units shipped.")
+        '
+        'lblEmployeeOne
+        '
+        Me.lblEmployeeOne.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmployeeOne.Location = New System.Drawing.Point(36, 98)
+        Me.lblEmployeeOne.Name = "lblEmployeeOne"
+        Me.lblEmployeeOne.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployeeOne.TabIndex = 3
+        Me.lblEmployeeOne.Text = "Employee 1"
+        Me.lblEmployeeOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblEmpoyeeTwo
+        '
+        Me.lblEmpoyeeTwo.Location = New System.Drawing.Point(180, 98)
+        Me.lblEmpoyeeTwo.Name = "lblEmpoyeeTwo"
+        Me.lblEmpoyeeTwo.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmpoyeeTwo.TabIndex = 4
+        Me.lblEmpoyeeTwo.Text = "Employee 2"
+        Me.lblEmpoyeeTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblEmployeeThree
+        '
+        Me.lblEmployeeThree.Location = New System.Drawing.Point(327, 98)
+        Me.lblEmployeeThree.Name = "lblEmployeeThree"
+        Me.lblEmployeeThree.Size = New System.Drawing.Size(82, 17)
+        Me.lblEmployeeThree.TabIndex = 5
+        Me.lblEmployeeThree.Text = "Employee 3"
+        Me.lblEmployeeThree.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtEmployeeOneResultDisplay
+        '
+        Me.txtEmployeeOneResultDisplay.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txtEmployeeOneResultDisplay.Location = New System.Drawing.Point(15, 130)
+        Me.txtEmployeeOneResultDisplay.Multiline = True
+        Me.txtEmployeeOneResultDisplay.Name = "txtEmployeeOneResultDisplay"
+        Me.txtEmployeeOneResultDisplay.ReadOnly = True
+        Me.txtEmployeeOneResultDisplay.Size = New System.Drawing.Size(134, 146)
+        Me.txtEmployeeOneResultDisplay.TabIndex = 6
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.txtEmployeeOneResultDisplay, "Displays the units shipped for employee 1 during the week.")
+        '
+        'txtEmployeeTwoResultDisplay
+        '
+        Me.txtEmployeeTwoResultDisplay.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txtEmployeeTwoResultDisplay.Location = New System.Drawing.Point(157, 130)
+        Me.txtEmployeeTwoResultDisplay.Multiline = True
+        Me.txtEmployeeTwoResultDisplay.Name = "txtEmployeeTwoResultDisplay"
+        Me.txtEmployeeTwoResultDisplay.ReadOnly = True
+        Me.txtEmployeeTwoResultDisplay.Size = New System.Drawing.Size(134, 146)
+        Me.txtEmployeeTwoResultDisplay.TabIndex = 7
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.txtEmployeeTwoResultDisplay, "Displays the units shipped for employee 2 during the week.")
+        '
+        'txtEmployeeThreeResultDisplay
+        '
+        Me.txtEmployeeThreeResultDisplay.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txtEmployeeThreeResultDisplay.Location = New System.Drawing.Point(300, 130)
+        Me.txtEmployeeThreeResultDisplay.Multiline = True
+        Me.txtEmployeeThreeResultDisplay.Name = "txtEmployeeThreeResultDisplay"
+        Me.txtEmployeeThreeResultDisplay.ReadOnly = True
+        Me.txtEmployeeThreeResultDisplay.Size = New System.Drawing.Size(134, 146)
+        Me.txtEmployeeThreeResultDisplay.TabIndex = 8
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.txtEmployeeThreeResultDisplay, "Displays the units shipped for employee 3 during the week.")
+        '
+        'lblEmployeeOneAverage
+        '
+        Me.lblEmployeeOneAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmployeeOneAverage.Location = New System.Drawing.Point(15, 279)
+        Me.lblEmployeeOneAverage.Name = "lblEmployeeOneAverage"
+        Me.lblEmployeeOneAverage.Size = New System.Drawing.Size(134, 29)
+        Me.lblEmployeeOneAverage.TabIndex = 9
+        Me.lblEmployeeOneAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.lblEmployeeOneAverage, "Displays the average for employee 1.")
+        '
+        'lblEmployeeTwoAverage
+        '
+        Me.lblEmployeeTwoAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmployeeTwoAverage.Location = New System.Drawing.Point(157, 279)
+        Me.lblEmployeeTwoAverage.Name = "lblEmployeeTwoAverage"
+        Me.lblEmployeeTwoAverage.Size = New System.Drawing.Size(134, 29)
+        Me.lblEmployeeTwoAverage.TabIndex = 10
+        Me.lblEmployeeTwoAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.lblEmployeeTwoAverage, "Displays the average for employee 2.")
+        '
+        'lblEmployeeThreeAverage
+        '
+        Me.lblEmployeeThreeAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmployeeThreeAverage.Location = New System.Drawing.Point(300, 279)
+        Me.lblEmployeeThreeAverage.Name = "lblEmployeeThreeAverage"
+        Me.lblEmployeeThreeAverage.Size = New System.Drawing.Size(134, 29)
+        Me.lblEmployeeThreeAverage.TabIndex = 11
+        Me.lblEmployeeThreeAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.lblEmployeeThreeAverage, "Displays the average for employee 3.")
+        '
+        'lblEmployeeTotalAverage
+        '
+        Me.lblEmployeeTotalAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmployeeTotalAverage.Location = New System.Drawing.Point(12, 324)
+        Me.lblEmployeeTotalAverage.Name = "lblEmployeeTotalAverage"
+        Me.lblEmployeeTotalAverage.Size = New System.Drawing.Size(419, 29)
+        Me.lblEmployeeTotalAverage.TabIndex = 12
+        Me.lblEmployeeTotalAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.lblEmployeeTotalAverage, "Displays the average of the total employees.")
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(15, 366)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(134, 32)
+        Me.btnEnter.TabIndex = 13
+        Me.btnEnter.Text = "&Enter"
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.btnEnter, "Click to add a daily unit entry for the employee." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10))
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(157, 366)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(134, 32)
+        Me.btnReset.TabIndex = 14
+        Me.btnReset.Text = "&Reset"
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.btnReset, "Click to reset the form." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10))
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(300, 366)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(134, 32)
+        Me.btnExit.TabIndex = 15
+        Me.btnExit.Text = "E&xit"
+        Me.ttAverageEmployeeUnitsShipped.SetToolTip(Me.btnExit, "Click to exit the form.")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmAverageUnitsShippedByEmployee
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(446, 413)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lblEmployeeTotalAverage)
+        Me.Controls.Add(Me.lblEmployeeThreeAverage)
+        Me.Controls.Add(Me.lblEmployeeTwoAverage)
+        Me.Controls.Add(Me.lblEmployeeOneAverage)
+        Me.Controls.Add(Me.txtEmployeeThreeResultDisplay)
+        Me.Controls.Add(Me.txtEmployeeTwoResultDisplay)
+        Me.Controls.Add(Me.txtEmployeeOneResultDisplay)
+        Me.Controls.Add(Me.lblEmployeeThree)
+        Me.Controls.Add(Me.lblEmpoyeeTwo)
+        Me.Controls.Add(Me.lblEmployeeOne)
+        Me.Controls.Add(Me.txtInputBox)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.lblDayDisplay)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageUnitsShippedByEmployee"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped By Employee"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblDayDisplay As Label
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents txtInputBox As TextBox
+    Friend WithEvents lblEmployeeOne As Label
+    Friend WithEvents lblEmpoyeeTwo As Label
+    Friend WithEvents lblEmployeeThree As Label
+    Friend WithEvents txtEmployeeOneResultDisplay As TextBox
+    Friend WithEvents txtEmployeeTwoResultDisplay As TextBox
+    Friend WithEvents txtEmployeeThreeResultDisplay As TextBox
+    Friend WithEvents lblEmployeeOneAverage As Label
+    Friend WithEvents lblEmployeeTwoAverage As Label
+    Friend WithEvents lblEmployeeThreeAverage As Label
+    Friend WithEvents lblEmployeeTotalAverage As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents ttAverageEmployeeUnitsShipped As ToolTip
+End Class
---
+++ AverageUnitsShippedByEmployee_ScottBrown/AverageUnitsShippedByEmployee.vb
@@ -0,0 +1,241 @@
+﻿'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'Name:                  Scott Brown
+'Date:                  2018-Mar-06
+'Course:                NETD-2202-01
+'Program Description:   This is a windows form application.  It takes the units shipped for a 
+'                       week, for 3 employees.  It will calculate the average for each 
+'                       employee and output it to the user.  Once the user inputs all the units
+'                       shipped for the employees, it will calculate the total average, for all the
+'                       employees.
+'     
+'Project Description:   This is the third lab for Net Development 1.  The main purpose of the 
+'                       lab is to implement 2D arrays.  The 2D array will store the units shipped
+'                       for each employee.  This program also uses many other concepts learned in 
+'                       the course such as looping, 1 dimension arrays, if/else statements,
+'                       methods and more.  Validation will also be implemented so the user has
+'                       to enter "proper" input.
+'
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+Option Strict On
+Public Class frmAverageUnitsShippedByEmployee
+    'Holds the value for the input from the user
+    Dim userInput As Integer
+
+    '2d array that holds the emloyees units shipped
+    Dim employeeArray(2, 6) As Integer
+    'Variable that holds the number of columns (used for looping through the array)
+    Dim numberOfColumns As Integer = employeeArray.GetLength(1)
+    'Variable that counts for each employee
+    Dim employeeCounter As Integer = 0
+
+    'Variable that holds the sum of the employees units
+    Dim unitSum As Double = 0.00
+    'Variable that holds the total for the units of all employees
+    Dim totalUnitSum As Double = 0.00
+    'Variable holding the value for the first employee's sum
+    Dim employeeOneSum As Double = 0.0
+    'Variable holding the value for the second employee's sum
+    Dim employeeTwoSum As Double = 0.0
+    'Variable holding the value for the third employee's sum
+    Dim employeeThreeSum As Double = 0.0
+    'Array that holds the values for the average
+    Dim employeeSumArray(2) As Integer
+    'Variable that counts the days
+    Dim dayCounter As Integer = 0
+    'Variable of the displayed day counter
+    Dim dayCounterDisplay As Integer = 1
+
+    ''' <summary>
+    ''' This method does the main calculations for the program.  When the button is clicked it will follow through the steps of the sub.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'Constant variables for the ranges and the days of the week
+        Const MaximumRange As Integer = 1000
+        Const MinimumRange As Integer = 0
+        Const DaysOfTheWeek As Integer = 7
+
+
+
+
+
+        'Error checking if statement making sure the value is an integer
+        If (Integer.TryParse(txtInputBox.Text, userInput) = False) Then
+            MessageBox.Show("Error, value must be an integer")
+
+            'Selecting the text box 
+            txtInputBox.SelectAll()
+            txtInputBox.Focus()
+
+            'Used to jump out if given a non integer value so the array doesn't include it
+            Return
+            ' If the user input is out of the range then give an error message
+        ElseIf (userInput > MaximumRange Or userInput < MinimumRange) Then
+            MessageBox.Show("Error, value must be between 0 and 1000 inclusive")
+            txtInputBox.SelectAll()
+
+
+            'Used to jump out if given a value not in range so the array doesn't include it
+            Return
+
+        Else
+            'Selecting the text box 
+            txtInputBox.Focus()
+            txtInputBox.SelectAll()
+            txtInputBox.Text = ""
+
+            'if the first employee is active then it will display the units in the first read only display text box
+            If (employeeCounter = 0) Then
+                txtEmployeeOneResultDisplay.Text &= (userInput & vbCrLf)
+                'if the second employee is active then it will display the units in the second read only display text box
+            ElseIf (employeeCounter = 1) Then
+                txtEmployeeTwoResultDisplay.Text &= (userInput & vbCrLf)
+                'if the third employee is active then it will display the units in the third read only display text box
+            Else
+                txtEmployeeThreeResultDisplay.Text &= (userInput & vbCrLf)
+            End If
+
+            'Making the row the employee count (0-2) and the day counter the column (0-6) and making it equal to the user input 
+            employeeArray(employeeCounter, dayCounter) = userInput
+            'Adding to the day counter if the result is good
+            dayCounter = dayCounter + 1
+            'adding to the day counter display
+            dayCounterDisplay = dayCounterDisplay + 1
+            'Making the day label display = to the day counter variable
+            lblDayDisplay.Text = "Day " + dayCounterDisplay.ToString()
+
+        End If
+        'Once it hits the end of the week it will go into this if
+        If dayCounter = 7 Then
+
+            'Making the day = to 7
+            dayCounterDisplay = dayCounterDisplay - 1
+            lblDayDisplay.Text = "Day " + dayCounterDisplay.ToString()
+
+            'Looping through the columns (7 days)
+            For index As Integer = 0 To numberOfColumns - 1
+                'adding the values together ' the for loop is doing the employee counter as the row and the index 
+                '(which increments by 1) as the column.
+                unitSum = unitSum + employeeArray(employeeCounter, index)
+            Next
+            'calculate the average
+            unitSum = (unitSum / DaysOfTheWeek)
+            'If it is employee 1
+            If (employeeCounter = 0) Then
+                'allowing employeeOneSum to be equal to the unit sum
+                employeeOneSum = unitSum
+
+                'storing the average into the array and casting it to a double variable
+                employeeSumArray(0) = CInt(CDbl(employeeOneSum))
+                'Outputting the average to the label and showing it to 2 decimal places
+                lblEmployeeOneAverage.Text = "Average: " + employeeOneSum.ToString("n2")
+
+                'If it is employee 2
+            ElseIf (employeeCounter = 1) Then
+                'allowing employeeOneSum to be equal to the unit sum
+                employeeTwoSum = unitSum
+                'storing the average into the array and casting it to a double variable
+                employeeSumArray(1) = CInt(CDbl(employeeTwoSum))
+
+                'Outputting the average to the label and showing it to 2 decimal places
+                lblEmployeeTwoAverage.Text = "Average: " + employeeTwoSum.ToString("n2")
+
+                'Once it hits employee three
+            Else
+                'allowing employeeOneSum to be equal to the unit sum
+                employeeThreeSum = unitSum
+                'Outputting the average to the label and showing it to 2 decimal places
+                lblEmployeeThreeAverage.Text = "Average: " + employeeThreeSum.ToString("n2")
+                'storing the average into the array and casting it to a double variable
+                employeeSumArray(2) = CInt(CDbl(employeeThreeSum))
+
+                'Making the enter button disables and making the text box read only
+                btnEnter.Enabled = False
+                txtInputBox.ReadOnly = True
+
+                'Lopping the array that holds the averages to get the total average
+                For index = 0 To employeeSumArray.Length - 1
+
+                    'adding the sum
+                    totalUnitSum = totalUnitSum + employeeSumArray(index)
+
+                Next
+                'calculating the total average
+                totalUnitSum = (totalUnitSum / 3)
+
+                'Displaying the total average to the label, two decimal places
+                lblEmployeeTotalAverage.Text = "Average per day: " + totalUnitSum.ToString("n2")
+
+
+
+
+            End If
+            'increase the emplopyee counter after the calculations finish for the previous employee
+            employeeCounter = employeeCounter + 1
+
+            'Setting the day counter back to 0
+            dayCounter = 0
+            'Setting the day counter display back to 1
+            dayCounterDisplay = 1
+            lblDayDisplay.Text = "Day 1"
+
+            'setting the sum back to 0
+            unitSum = 0
+            Return
+
+        End If
+
+    End Sub
+    ''' <summary>
+    ''' Resets the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        Me.ButtonReset()
+    End Sub
+    ''' <summary>
+    ''' Closes the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the form
+        Me.Close()
+    End Sub
+    ''' <summary>
+    ''' Method to reset the form
+    ''' </summary>
+    Private Sub ButtonReset()
+
+        'Ressetting all text boxes, labels, buttons and variables back to their orginal state
+        btnEnter.Enabled = True
+        txtInputBox.ReadOnly = False
+        txtInputBox.Focus()
+        txtInputBox.Text = ""
+        employeeCounter = 0
+        dayCounter = 0
+        dayCounterDisplay = 1
+        lblDayDisplay.Text = "Day 1"
+        unitSum = 0.00
+        totalUnitSum = 0.00
+        employeeOneSum = 0.0
+        employeeTwoSum = 0.0
+        employeeThreeSum = 0.0
+
+        txtEmployeeOneResultDisplay.Text = ""
+        txtEmployeeTwoResultDisplay.Text = ""
+        txtEmployeeThreeResultDisplay.Text = ""
+        lblEmployeeOneAverage.Text = ""
+        lblEmployeeTwoAverage.Text = ""
+        lblEmployeeTotalAverage.Text = ""
+        lblEmployeeThreeAverage.Text = ""
+        lblEmployeeTotalAverage.Text = ""
+
+
+    End Sub
+
+
+End Class
